c5bed88ecede9b9efcb302f233aca9425f42431b
Nahuel Escudero <escuderogomezn@gmail.com>
2024-02-29 17:11:16 -0300

    Completo necesidades desafío 2

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ class ProductManager {
 
       this.products.push(product);
 
-      const prodsInJsonString = JSON.stringify(this.products);
+      const prodsInJsonString = JSON.stringify(this.products, null, "\t");
       const productsPath = "./productos.json";
 
       await fs.promises.writeFile(productsPath, prodsInJsonString);
@@ -59,7 +59,10 @@ class ProductManager {
     } catch (error) {
       if (error.code === "ENOENT") {
         // Si el archivo no existe, creo un array vacío y lo escribo en el archivo
-        await fs.promises.writeFile(productsPath, JSON.stringify(this.products));
+        await fs.promises.writeFile(
+          productsPath,
+          JSON.stringify(this.products, null, "\t")
+        );
         console.log("Archivo de productos creado");
         return [];
       } else {
@@ -80,6 +83,7 @@ class ProductManager {
     }
   }
 
+  //Metodo que actualiza un producto en productos.json
   async updateProduct(id, object) {
     try {
       const prodsJsonString = await fs.promises.readFile(
@@ -87,7 +91,6 @@ class ProductManager {
         "utf-8"
       );
       const prodsJsonParse = JSON.parse(prodsJsonString);
-      console.log(prodsJsonParse);
 
       const index = prodsJsonParse.findIndex((product) => product.id === id);
 
@@ -96,13 +99,42 @@ class ProductManager {
 
         await fs.promises.writeFile(
           "./productos.json",
-          JSON.stringify(prodsJsonParse, null)
+          JSON.stringify(prodsJsonParse, null, "\t")
         );
       } else {
         console.error("Producto no modificado (no se encontró)");
       }
     } catch (error) {
       console.error(`Error al actualizar el producto : ${error}`);
+    }
+  }
+
+  //Elimino producto obtenido por id de productos.json
+  async deleteProduct(id) {
+    const productsPath = "./productos.json";
+
+    try {
+      const prodsJsonString = await fs.promises.readFile(
+        "./productos.json",
+        "utf-8"
+      );
+      const prodsJsonParse = JSON.parse(prodsJsonString);
+
+      const index = prodsJsonParse.findIndex((product) => product.id === id);
+
+      if (index !== -1) {
+        prodsJsonParse.splice(index, 1); // Elimino el producto del array
+        await fs.promises.writeFile(
+          productsPath,
+          JSON.stringify(prodsJsonParse, null, "\t")
+        ); // Escribo el array actualizado al archivo
+
+        console.log("Producto eliminado correctamente");
+      } else {
+        console.error("Producto no encontrado");
+      }
+    } catch (error) {
+      console.error(`No se pudo eliminar el producto: ${error}`);
     }
   }
 }
@@ -112,67 +144,81 @@ class ProductManager {
 //Creo instancia del manejador de productos
 const manager = new ProductManager();
 
-// //LLamo a getProducts
-console.log(manager.getProducts()); //[]
+//Función que ejecuta todas las funcionalidades de los métodos del ProductManager
+const init = async () => {
+  //LLamo a getProducts
+  console.log("Contenedor de productos: ", await manager.getProducts()); //[]
 
-//Agrego productos con addProduct;
-manager.addProduct(
-  "Remera",
-  "REMERA EFECTO INVERNADERO [ NEVADO ]",
-  40000,
-  "https://firebasestorage.googleapis.com/v0/b/insidia-ind.appspot.com/o/remeras%2FREMERA%20EFECTO%20INVERNADERO%20%5B%20NEVADO%20%5D.png?alt=media&token=5fcd5ef4-cb88-4433-96b4-44c99e721f37",
-  "R1",
-  10
-);
+  //Agrego productos con addProduct;
+  await manager.addProduct(
+    "Remera",
+    "REMERA EFECTO INVERNADERO [ NEVADO ]",
+    40000,
+    "https://firebasestorage.googleapis.com/v0/b/insidia-ind.appspot.com/o/remeras%2FREMERA%20EFECTO%20INVERNADERO%20%5B%20NEVADO%20%5D.png?alt=media&token=5fcd5ef4-cb88-4433-96b4-44c99e721f37",
+    "R1",
+    10
+  );
 
-manager.addProduct(
-  "Remera",
-  "REMERA ENERGÍAS RENOVABLES [ NEGRO ]",
-  38000,
-  "https://firebasestorage.googleapis.com/v0/b/insidia-ind.appspot.com/o/remeras%2FREMERA%20ENERG%C3%8DAS%20RENOVABLES%20%5B%20NEGRO%20%5D.png?alt=media&token=8e3be9bf-e046-4a3c-843a-99ba97d1b2af",
-  "R2",
-  15
-);
+  await manager.addProduct(
+    "Remera",
+    "REMERA ENERGÍAS RENOVABLES [ NEGRO ]",
+    38000,
+    "https://firebasestorage.googleapis.com/v0/b/insidia-ind.appspot.com/o/remeras%2FREMERA%20ENERG%C3%8DAS%20RENOVABLES%20%5B%20NEGRO%20%5D.png?alt=media&token=8e3be9bf-e046-4a3c-843a-99ba97d1b2af",
+    "R2",
+    15
+  );
 
-manager.addProduct(
-  "Remera",
-  "REMERA SUNWAVES [ LS08 ]",
-  45000,
-  "https://firebasestorage.googleapis.com/v0/b/insidia-ind.appspot.com/o/remeras%2FREMERA%20SUNWAVES%20%5B%20LS08%20%5D.png?alt=media&token=494f2bbc-6806-4467-81a8-db7e367fde33",
-  "R3",
-  12
-);
+  await manager.addProduct(
+    "Remera",
+    "REMERA SUNWAVES [ LS08 ]",
+    45000,
+    "https://firebasestorage.googleapis.com/v0/b/insidia-ind.appspot.com/o/remeras%2FREMERA%20SUNWAVES%20%5B%20LS08%20%5D.png?alt=media&token=494f2bbc-6806-4467-81a8-db7e367fde33",
+    "R3",
+    12
+  );
 
-manager.addProduct(
-  "Remera",
-  "REMERA SUNWAVES [ LS15 ]",
-  35000,
-  "https://firebasestorage.googleapis.com/v0/b/insidia-ind.appspot.com/o/remeras%2FREMERA%20SUNWAVES%20%5B%20LS15%20%5D.png?alt=media&token=7b4d7c6e-a310-4f6a-8be5-2e4e44cde4c0",
-  "R4",
-  8
-);
+  await manager.addProduct(
+    "Remera",
+    "REMERA SUNWAVES [ LS15 ]",
+    35000,
+    "https://firebasestorage.googleapis.com/v0/b/insidia-ind.appspot.com/o/remeras%2FREMERA%20SUNWAVES%20%5B%20LS15%20%5D.png?alt=media&token=7b4d7c6e-a310-4f6a-8be5-2e4e44cde4c0",
+    "R4",
+    8
+  );
 
-//Vuelvo a llamar a getProducts, esta vez con productos dentro
-// console.log(manager.getProducts()); //[productos]
+  //Vuelvo a llamar a getProducts, esta vez con productos dentro
+  console.log("Contenedor de productos: ", await manager.getProducts()); //[productos]
 
-//Agrego el 4to producto otra vez, lo que devolverá un error
-// manager.addProduct(
-//   "Remera",
-//   "REMERA SUNWAVES [ LS15 ]",
-//   35000,
-//   "https://firebasestorage.googleapis.com/v0/b/insidia-ind.appspot.com/o/remeras%2FREMERA%20SUNWAVES%20%5B%20LS15%20%5D.png?alt=media&token=7b4d7c6e-a310-4f6a-8be5-2e4e44cde4c0",
-//   "R4",
-//   8
-// );
+  //Agrego el 4to producto otra vez, lo que devolverá un error
+  await manager.addProduct(
+    "Remera",
+    "REMERA SUNWAVES [ LS15 ]",
+    35000,
+    "https://firebasestorage.googleapis.com/v0/b/insidia-ind.appspot.com/o/remeras%2FREMERA%20SUNWAVES%20%5B%20LS15%20%5D.png?alt=media&token=7b4d7c6e-a310-4f6a-8be5-2e4e44cde4c0",
+    "R4",
+    8
+  );
 
-//Busco producto 1
-// console.log(manager.getProductById(1));
+  //Busco producto 1
+  console.log("Producto buscado: ", await manager.getProductById(1));
 
-// Busco producto inexistente
-// console.log(manager.getProductById(5));
+  // Busco producto inexistente
+  console.log(manager.getProductById(5));
 
-//Modifico producto
-// manager.updateProduct(4, {title: "Remera modificada", description: "Remera modificada con updateProduct"});
+  //Modifico producto
+  await manager.updateProduct(4, {
+    title: "Remera modificada",
+    description: "Remera modificada con updateProduct",
+  });
 
-//Vuelvo a llamar a getProducts, esta vez con productos dentro
-// console.log(manager.getProducts()); //[productos]
+  //Vuelvo a llamar a getProducts
+  console.log("Productos modificados: ", await manager.getProducts()); //[productos]
+
+  //Elimino el producto 2
+  await manager.deleteProduct(2);
+
+  //Vuelvo a llamar a getProducts
+  console.log("Productos modificados: ", await manager.getProducts()); //[productos]
+};
+
+init();
