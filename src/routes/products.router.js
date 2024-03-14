70ea6b1ac4cea96f6330b3c556c46dc8f62f1080
Nahuel Escudero <escuderogomezn@gmail.com>
2024-03-13 22:34:53 -0300

    Termino necesidades de la ruta /api/products

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -71,4 +71,32 @@ router.post("/", async (req, res) => {
   res.status(201).send({ message: "Producto creado correctamente!" });
 });
 
+router.put("/:productId", async (req, res) => {
+  const id = parseInt(req.params.productId);
+  const { title, description, code, price, stock, category /*thumbnails*/ } =
+    req.body;
+
+  const newProduct = {
+    title,
+    description,
+    code,
+    price,
+    status: true,
+    stock,
+    category,
+    // thumbnails,
+  };
+
+  await PM.updateProduct(id, newProduct);
+  res.status(201).send({ message: "Producto modificado correctamente!" });
+});
+
+router.delete("/:productId", async (req, res) => {
+  const id = parseInt(req.params.productId);
+
+  await PM.deleteProduct(id);
+
+  res.status(200).send({message: "Producto eliminado correctamente"});
+});
+
 export default router;
